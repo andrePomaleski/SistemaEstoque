53a2bee51d33a90f37b4dd4db261f892d60f37dc
André Pomaleski <andrepomaleski@hotmail.com>
2024-12-14 20:47:13 -0300

    Feat: added Product requestDTO

---
+++ src/main/java/com/andrePomaleski/SistemaEstoque/dto/ProductRequestDTO.java
@@ -0,0 +1,38 @@
+package com.andrePomaleski.SistemaEstoque.dto;
+
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductRequestDTO {
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotNull(message = "Quantity cannot be null")
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private Integer quantity;
+
+    @NotNull(message = "Price cannot be null")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
+    private BigDecimal price;
+
+    @NotNull(message = "Supplier ID cannot be null")
+    private Long supplierId;
+
+    @NotEmpty(message = "Category IDs cannot be empty")
+    private List<Long> categoryIds;
+}
