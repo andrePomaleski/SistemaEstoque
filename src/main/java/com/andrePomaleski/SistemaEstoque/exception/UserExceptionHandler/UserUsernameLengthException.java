40cfcbe1be2db516a7b8a238b8fdf0d3df64e35b
André Pomaleski <andrepomaleski@hotmail.com>
2024-12-20 02:09:09 -0300

    Feat: added UserUsernameLengthException

---
+++ src/main/java/com/andrePomaleski/SistemaEstoque/exception/UserExceptionHandler/UserUsernameLengthException.java
@@ -0,0 +1,10 @@
+package com.andrePomaleski.SistemaEstoque.exception.UserExceptionHandler;
+
+public class UserUsernameLengthException extends Exception{
+    public UserUsernameLengthException() {
+    }
+
+    public UserUsernameLengthException(String message) {
+        super(message);
+    }
+}
